605965401c20c012940948352b3c349fd1c525e2
sireeshajangam <sireesha.jangam@auropro.com>
2023-11-15 11:54:02 +0530

    committed

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-set -e
 
-# Stop the running container (if any).
-echo "Hi"
+# Identify the running container ID or name
+CONTAINER_ID=$(docker ps -qf "ancestor=571888835380.dkr.ecr.us-east-1.amazonaws.com/python-repo:latest")
+
+# Check if a container is running
+if [ -n "$CONTAINER_ID" ]; then
+    # Stop the running container
+    docker stop "$CONTAINER_ID"
+    echo "Container stopped successfully."
+else
+    echo "No running container found for the specified image."
+fi
